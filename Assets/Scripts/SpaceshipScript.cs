8f7d84d1d50dccbdda715ccf776fc3ab79700bda
Duarte Jeremias <50424889+duartejeremias@users.noreply.github.com>
2023-10-17 15:50:12 +0100

    added collision detection
    added collision detection between asteroids and the spaceship

--- Assets/Scripts/SpaceshipScript.cs
+++ Assets/Scripts/SpaceshipScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class SpaceshipScript : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,15 @@ public class SpaceshipScript : MonoBehaviour
         float moveVertical = Input.GetAxis ("Vertical");
  
         rb.velocity = new Vector2 (moveHorizontal*speed, moveVertical*speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("Asteroid")) {
+            Debug.Log("Collision!");
+            
+            Destroy(other.gameObject);
+            
+            this.gameObject.SetActive(false);
+        }
     }
 }
